2a5ee195304408a2ed7086a137d93c409729b9a1
ULemong <maintain9912@gmail.com>
2021-07-02 23:59:38 +0900

    숫자 카드 게임

---
+++ Ch03_그리디/3-3.py
@@ -0,0 +1,24 @@
+#1. min()함수 이용#
+n, m = map(int, input().split())
+b = []
+
+for i in range(n):
+    a = list(map(int, input().split()))
+    s = min(a)
+    b.append(s)
+
+print(max(b))
+
+#2. 이중 for문 이용#
+n, m = map(int, input().split())
+b = []
+
+for i in range(n):
+    a = list(map(int, input().split()))
+    s = a[0]
+    for j in range(m):
+        if a[j] < s:
+            s = a[j]
+    b.append(s)
+
+print(max(b))
